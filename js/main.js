7fd2b29e3571292e54f1aeb0c4acc8698ceb24db
Tonderai Mutanda <tonderaimutanda@gmail.com>
2019-02-17 19:54:46 +0200

    Updated footer

--- js/main.js
+++ js/main.js
@@ -79,3 +79,15 @@ jQuery(document).ready(function($) {
 	});
 
 });
+
+
+if( jQuery(".toggle .toggle-title").hasClass('active') ){
+  jQuery(".toggle .toggle-title.active").closest('.toggle').find('.toggle-inner').show();
+}
+jQuery(".toggle .toggle-title").click(function(){
+  if( jQuery(this).hasClass('active') ){
+    jQuery(this).removeClass("active").closest('.toggle').find('.toggle-inner').slideUp(200);
+  }
+  else{	jQuery(this).addClass("active").closest('.toggle').find('.toggle-inner').slideDown(200);
+  }
+});
